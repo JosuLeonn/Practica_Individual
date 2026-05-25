99fc7f4ea0461be994f5439de76c8c9f675ba45a
Josué de León <josue.leongarcia28@gmail.com>
2026-05-25 09:59:13 -0600

    FEAT Add question of the user

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,5 +2,8 @@ import java.util.Scanner;
 public class HolaMundo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("----Bienvenido al Programa User----\n----Identificate----");
+        System.out.println("Ingresa tu usuario");
+        String user = sc.nextLine();
     }
 }
