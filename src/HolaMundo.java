54eef6c1d18bf2435effc2258368688e820a67d1
Josué de León <josue.leongarcia28@gmail.com>
2026-05-27 08:27:27 -0600

    FEAT Create password question interface

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,7 +3,15 @@ public class HolaMundo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("----Bienvenido al Programa User----\n----Identificate----");
-        System.out.println("Ingresa tu usuario");
+        System.out.println("Ingresa tu Usuario");
         String user = sc.nextLine();
+        System.out.println("Ingresa tu contraseña");
+        int contra = sc.nextInt();
+        if (contra == 2026){
+            System.out.println("Contraseña correcta\nBienvenido Transformador: " + user);
+        }
+        else{
+            System.out.println("Contraseña incorrecta ¡Fuera de aca impostor!");
+        }
     }
 }
